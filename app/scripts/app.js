d3d740ce96d2770769e71802c0e0cc5951f43238
Cameron Dumas <cdumas22@gmail.com>
2014-05-29 16:54:44 -0500

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+angular.module('bookmarksApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute','xeditable','snap','angular-growl', 'ui.bootstrap','ngAnimate','ngDragDrop', 'ngTouch', 'ngResource', 'ngTagsInput', 'wu.masonry'
+])
+  .config(function ($routeProvider, $locationProvider, $httpProvider, growlProvider) {
+    growlProvider.globalTimeToLive(3000);
+    growlProvider.onlyUniqueMessages(false);  
+    
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main'
+      })
+      .when('/login', {
+        templateUrl: 'partials/login',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'partials/signup',
+        controller: 'SignupCtrl'
+      })
+      .when('/settings', {
+        templateUrl: 'partials/settings',
+        controller: 'SettingsCtrl',
+        authenticate: true
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+      
+    $locationProvider.html5Mode(true);
+      
+    // Intercept 401s and redirect you to login
+    $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
+      return {
+        'responseError': function(response) {
+          if(response.status === 401) {
+            $location.path('/login');
+            return $q.reject(response);
+          }
+          else {
+            return $q.reject(response);
+          }
+        }
+      };
+    }]);
+  })
+  .run(function ($rootScope, $location, Auth, editableOptions) {
+      editableOptions.theme = 'bs3';
+    // Redirect to login if route requires auth and you're not logged in
+    $rootScope.$on('$routeChangeStart', function (event, next) {
+      
+      if (next.authenticate && !Auth.isLoggedIn()) {
+        $location.path('/login');
+      }
+    });
+  });
